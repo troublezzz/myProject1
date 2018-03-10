efd512de280001f5ac5e3ca38d2b36d30a58d780
O00281 <O00281@vdesktop.huawei.com>
2018-03-10 18:56:02 +0800

    20180310

---
+++ js/common.js
@@ -0,0 +1,13 @@
+/**
+ * Created by O00281 on 2018/3/8.
+ */
+
+
+function dropdownOpen() {
+    var $dropdownLi = $('li.dropdown');
+    $dropdownLi.mouseover(function() {
+        $(this).addClass('open');
+    }).mouseout(function() {
+        $(this).removeClass('open');
+    });
+}
